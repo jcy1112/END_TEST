2cc69d6f86be5d94cdb4870b384b2a1e6906e16c
root <1471081165@qq.com>
2023-03-20 10:13:32 +0800

    add anxujiazai

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+import './plugins/element.js'
 import './assets/gloable.css'
 import request from "@/utils/request";
 import store from './store'
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI,{size:"mini"});
+// Vue.use(ElementUI,{size:"mini"});
 
 Vue.config.http = request
 
